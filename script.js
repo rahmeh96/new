619f30cbbde3073ff6a5092a25ef6836e0f029b4
rahmeh96 <143027366+rahmeh96@users.noreply.github.com>
2023-12-19 12:50:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function missingNumber(nums) {
+    let missing = nums.length; 
+  
+    for (let i = 0; i < nums.length; i++) {
+      missing ^= i ^ nums[i];
+    }
+  
+    return missing;
+  }
+  
+  
